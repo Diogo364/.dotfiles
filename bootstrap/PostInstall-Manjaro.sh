e4c3471f7e98fbeb3be087852e2a251e0ef8dca4
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-05-21 05:44:52 -0300

    Clean PostInstall Manjaro shellscript

--- bootstrap/PostInstall-Manjaro.sh
+++ bootstrap/PostInstall-Manjaro.sh
@@ -22,7 +22,6 @@ sudo pacman -S \
 	neofetch \
 	gufw \
 	flatpak \
-	gnome-software-packagekit-plugin \
 	ffmpeg \
 	base-devel \
 	yay \
@@ -30,25 +29,3 @@ sudo pacman -S \
 	curl \
 	wget \
 	brave-browser
-
-# Rust
-echo "Installing Rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-#Pop OS Tiling tool
-echo "Installing Pop OS Tiling Tool"
-yay -S gnome-shell-extension-pop-shell
-
-# Pop OS Launcher
-echo "Installing Pop OS Launcher"
-git clone https://github.com/pop-os/launcher.git
-cd launcher
-just build-release # Build
-just install # Install locally
-cd ~
-rm -rf launcher
-
-# Neovim Configurations
-echo "Getting neovim configuration"
-git clone https://github.com/Diogo364/my-nvim.git 
-mv my-nvim /home/diogotn/.config/nvim
