6ae730044749e03a10ea7eb4e881ee5aa746317d
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-11-05 08:39:25 -0400

    Add pip completions to zsh

--- .config/zsh/custom/completions.sh
+++ .config/zsh/custom/completions.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 zstyle ':completion:*:*:docker:*' option-stacking yes
 zstyle ':completion:*:*:docker-*:*' option-stacking yes
+eval "`pip completion --zsh`"
