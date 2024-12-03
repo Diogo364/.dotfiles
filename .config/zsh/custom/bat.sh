7f72b0200062190e1511fa8afe599324dcb13fe7
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-12-03 18:05:18 -0400

    Update Bat zsh config

--- .config/zsh/custom/bat.sh
+++ .config/zsh/custom/bat.sh
@@ -1,8 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-if ! [[ -s $(command -v bat) ]]; then
-    _verbose_notify "binary bat not found"
-    return
-fi
+[[ -s $(command -v bat) ]] || $(_verbose_notify "binary bat not found" && return)
 
-export BAT_THEME="Monokai Extended Origin"
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+
+alias bathelp='bat --language=help'
+
+help() {
+    "$@" -h 2>&1 | bathelp
+}
+
+alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
