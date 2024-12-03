ea3f8a4f1e83fc4afd8529feefa782220635b427
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-12-03 18:03:50 -0400

    Update zoxide zsh config

--- .config/zsh/custom/zoxide.sh
+++ .config/zsh/custom/zoxide.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-if ! [[ -s $(command -v zoxide) ]]; then
-    _verbose_notify "binary zoxide not found"
-    return
-fi
+[[ -s $(command -v zoxide) ]] || $(_verbose_notify "binary zoxide not found" && return)
 
 eval "$(zoxide init zsh)"
-# alias cd="z"
 
-fpath+=${ZDOTDIR:-~}/.zsh_functions
+fpath+=${ZDOTDIR}/.zsh_functions
