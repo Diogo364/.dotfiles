2c1dbb24d45c932bc7a02b593a8ce96005ed2e61
Diogo Telheiro do Nascimento <44041957+Diogo364@users.noreply.github.com>
2024-12-03 18:04:53 -0400

    Update eza zsh config

--- .config/zsh/custom/eza.sh
+++ .config/zsh/custom/eza.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-if ! [[ -s $(command -v eza) ]]; then
-    _verbose_notify "binary eza not found"
-    return
-fi
+[[ -s $(command -v eza) ]] || $(_verbose_notify "binary eza not found" && return)
 
 # ---- Eza (better ls) -----
-alias l="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+alias l="eza --color=always --long --no-filesize --no-time --no-user --no-permissions"
 alias ll="eza --color=always --long --git --icons=always --header" 
+alias la="eza --color=always -la --git --icons=always --header" 
 
 show_file_or_dir_preview="if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi"
